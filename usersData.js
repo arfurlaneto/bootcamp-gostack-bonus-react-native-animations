df00138c8a2caf475bc21ef4ade39b9b133cf919
arfurlaneto <arfurlaneto@gmail.com>
2019-11-06 03:26:15 -0300

    New user data

--- usersData.js
+++ usersData.js
@@ -1,9 +1,9 @@
 export default [
   {
     id: 1,
-    name: 'Diego Fernandes',
-    description: 'Head de programação!',
-    avatar: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4',
+    name: 'Antonio Roberto Furlaneto',
+    description: 'Back-end Developer',
+    avatar: 'https://avatars0.githubusercontent.com/u/3330854?s=460&v=4',
     thumbnail:
       'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=400&q=80',
     likes: 200,
@@ -11,9 +11,9 @@ export default [
   },
   {
     id: 2,
-    name: 'Robson Marques',
-    description: 'Head de empreendedorismo!',
-    avatar: 'https://avatars2.githubusercontent.com/u/861751?s=460&v=4',
+    name: 'Victor Bastos',
+    description: 'Front-end Developer',
+    avatar: 'https://avatars1.githubusercontent.com/u/1017515?s=460&v=4',
     thumbnail:
       'https://images.unsplash.com/photo-1490633874781-1c63cc424610?auto=format&fit=crop&w=400&q=80',
     likes: 350,
@@ -21,9 +21,9 @@ export default [
   },
   {
     id: 3,
-    name: 'Cleiton Souza',
-    description: 'Head de mindset!',
-    avatar: 'https://avatars0.githubusercontent.com/u/4669899?s=460&v=4',
+    name: 'Renan Muliterno Ferreira',
+    description: 'Developer in Ascension',
+    avatar: 'https://avatars2.githubusercontent.com/u/45672566?s=460&v=4',
     thumbnail:
       'https://images.unsplash.com/photo-1506440905961-0ab11f2ed5bc?auto=format&fit=crop&w=400&q=80',
     likes: 250,
@@ -31,9 +31,9 @@ export default [
   },
   {
     id: 4,
-    name: 'Robson Marques',
-    description: 'Head de empreendedorismo!',
-    avatar: 'https://avatars2.githubusercontent.com/u/861751?s=460&v=4',
+    name: 'Thiago Ribeiro',
+    description: 'Team Manager',
+    avatar: 'https://avatars3.githubusercontent.com/u/4700495?s=460&v=4',
     thumbnail:
       'https://images.unsplash.com/photo-1490633874781-1c63cc424610?auto=format&fit=crop&w=400&q=80',
     likes: 350,
